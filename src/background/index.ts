c7b06037d00fe5337f352ab1dadbd1a337bad669
adshafqat <adshafqat@yahoo.com>
2023-02-01 15:36:41 +0000

    Update index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -17,12 +17,14 @@ async function postData(body : any){
   headers,
  };
 
-  const URL = "http://localhost:4318/v1/traces";
-  console.log("Posting the data");
+  //const URL = "http://localhost:4318/v1/traces";
+  const URL = "http://localhost:8080/traces";
+  console.log("Posting the data to the backend application");
+  //console.log("Posting the data");
   console.log(request);
   //const reqq_grpc = value.encode(body);
   let  page = await fetch(URL, request);
   let json = page.json();
-  console.log("Data recieved from backend \n");
+  console.log("Data recieved from backend v1.3 \n");
   console.log(page);
 }
